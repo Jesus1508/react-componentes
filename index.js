b62f7d905fee484f2c69b79a59cb9a33b285cf0c
Jesus1508 <iscjesus1508@gmail.com>
2022-07-03 15:21:54 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,72 +1,87 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { render } from 'react-dom';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
 
-class App extends Component {
-  static defaultProps = {
-    nme: 'Componente Jesus',
-  };
+let Example = (props) => {
+  let [contador, setContador] = useState(0);
+  useEffect(() => {
+    console.log('Fui Actualizado useEffect');
+  }, []);
 
-  static propTypes = {
-    name: propTypes.string,
-  };
+  return (
+    <>
+      <p>Contador: {this.state.contador}</p>
+      <button onClick={setContador(contador + 1)}>Sumar</button>
+    </>
+  );
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      contador: 0,
-      updateAt: null,
-      intv: null,
-    };
-  }
+// class App extends Component {
+// static defaultProps = {
+//   nme: 'Componente Jesus',
+// };
 
-  componentDidMount() {
-    console.log('Fui creado!!');
-    // let intervalo = setInterval(
-    //   () => this.setState({ contador: this.state.contador + 1 }),
-    //   1000
-    // );
+// static propTypes = {
+//   name: propTypes.string,
+// };
 
-    // this.setState({
-    //   intv: intervalo,
-    // });
-  }
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     contador: 0,
+//     updateAt: null,
+//     intv: null,
+//   };
+// }
 
-  componentDidUpdate(prevProps, prevState) {
-    // console.log(prevState, this.state);
-    // if (prevState.contador !== this.state.contador) {
-    //   this.setState({
-    //     updateAt: new Date(),
-    //   });
-    // }
-    // console.log('Actualizado!!');
-  }
+// componentDidMount() {
+//   console.log('Fui creado!!');
+//   let intervalo = setInterval(
+//     () => this.setState({ contador: this.state.contador + 1 }),
+//     1000
+//   );
 
-  // getSnapshotBeforeUpdate() {
-  //   console.log('Antes de actualizar DOM');
-  //   return 3;
-  // }
+//   this.setState({
+//     intv: intervalo,
+//   });
+// }
 
-  componentWillUnmount() {
-    // alert('Adios');
-    // clearInterval(this.state.intv);
-  }
+// componentDidUpdate(prevProps, prevState) {
+// console.log(prevState, this.state);
+// if (prevState.contador !== this.state.contador) {
+//   this.setState({
+//     updateAt: new Date(),
+//   });
+// }
+// console.log('Actualizado!!');
+// }
 
-  updateCounter = () => {
-    this.setState({ contador: this.state.contador + 1 });
-  };
+// getSnapshotBeforeUpdate() {
+//   console.log('Antes de actualizar DOM');
+//   return 3;
+// }
 
-  render() {
-    // console.log(this.props);
-    return (
-      <>
-        <p>Contador: {this.state.contador}</p>
-        <button onClick={this.updateCounter}>Sumar</button>
-      </>
-    );
-  }
-}
+// componentWillUnmount() {
+// alert('Adios');
+// clearInterval(this.state.intv);
+// }
 
-render(<App />, document.getElementById('root'));
+// updateCounter = () => {
+//   this.setState({ contador: this.state.contador + 1 });
+// };
+
+// render() {
+// console.log(this.props);
+//   return (
+//     <>
+//       <p>Contador: {this.state.contador}</p>
+//       <button onClick={this.updateCounter}>Sumar</button>
+//     </>
+//   );
+// }
+// }
+
+render(<Example />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
 // render(<App name={'Jesus'} />, document.getElementById('root'));
