66c7d1c3e83d4bda92c1f1dda4aa6195f37731d0
Jesus1508 <iscjesus1508@gmail.com>
2022-07-03 14:09:02 -0500

    1 file modified

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+  static defaultProps = {
+    nme: 'Componente Jesus',
+  };
+
   render() {
     console.log(this.props);
     return <p>Hola mundo</p>;
   }
 }
 
-render(<App name={'Jesus'} />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
+// render(<App name={'Jesus'} />, document.getElementById('root'));
