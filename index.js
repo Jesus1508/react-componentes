d92bfa26c69d53912e5a2f69fb1dbf1c828337e0
Jesus1508 <iscjesus1508@gmail.com>
2022-07-03 14:04:51 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { Component } from 'react';
 import { render } from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
   render() {
+    console.log(this.props);
     return <p>Hola mundo</p>;
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App name={'Jesus'} />, document.getElementById('root'));
