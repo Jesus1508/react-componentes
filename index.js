cec48b726b69a06f23bd47316b1a1b7c7d54316c
Jesus1508 <iscjesus1508@gmail.com>
2022-07-03 14:41:53 -0500

    1 file modified

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
     super(props);
     this.state = {
       contador: 0,
-      updateAt = null
+      updateAt: null,
     };
   }
 
@@ -26,10 +26,10 @@ class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     console.log(prevState, this.state);
-    if (prevState.contador !== this.state.contador){
+    if (prevState.contador !== this.state.contador) {
       this.setState({
-        updateAt: new Date()
-      })
+        updateAt: new Date(),
+      });
     }
     console.log('Actualizado!!');
   }
