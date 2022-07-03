ae3a5735b53dfae7656646dca6ed0df8c826a92c
Jesus1508 <iscjesus1508@gmail.com>
2022-07-03 14:27:16 -0500

    1 file modified

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ class App extends Component {
     };
   }
 
+  updateCounter() {
+    this.setState({ contador: this.state.contador + 1 });
+  }
+
   render() {
     console.log(this.props);
     return (
       <>
         <p>Contador: {this.state.contador}</p>
-        <button
-          onClick={() => this.setState({ contador: this.state.contador + 1 })}
-        >
-          Sumar
-        </button>
+        <button onClick={() => this.updateCounter}>Sumar</button>
       </>
     );
   }
